bda04f5d4f207e265e1edbeabdbbbb54702bede2
Omar Gonzalez <om.gonzalez007@gmail.com>
2024-01-18 18:34:31 -0500

    Add files via upload
    Adding while true and other features

--- cowandbull.py
+++ cowandbull.py
@@ -13,16 +13,25 @@ def guessed_number(npclist, userlist):
 
 
 npclist = [random.randint(0,9) for i in range(0,4)]
-#print(npclist)
+    #print(npclist)
 
+rounds = 1
+cow, bull = (0,0)    
+while True:
+    userlist = input("Enter four digits(You have five round): ")
+    if rounds == 5:
+        print("You complete the five rounds - Good Bye!")
+        print(f"The 4 digits were {npclist}")
+        break
+    else:
+        userlist = list(userlist)
+        userlist = [int(i) for i in userlist]
+        result = guessed_number(npclist, userlist)
+        cow += result[0]
+        bull += result[1]
+        print(f"cow: {cow} - bull: {bull}  - You have {rounds} rounds")
 
-userlist = input("Enter four digits: ")
-userlist = list(userlist)
-userlist = [int(i) for i in userlist]
-
-cow , bull = guessed_number(npclist, userlist)
-print(f"cow: {cow} - bull: {bull}")
-
+    rounds += 1
 
 
 
